fee19c268f2dd7a47ca80058f2cf2288a50842d8
CallanGeorge <callangeorge19@gmail.com>
2025-07-29 14:35:39 +0100

    better mobile nav and services drop down

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,12 +6,18 @@ import styles from './Navbar.module.css'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
+  const [isServicesOpen, setIsServicesOpen] = useState(false)
+  const [isMobileServicesOpen, setIsMobileServicesOpen] = useState(false)
 
   const navItems = [
     { name: 'ABOUT', href: '/about' },
     { name: 'WORK', href: '/work' },
     { name: 'BLOG', href: '#blog' },
-    { name: 'SERVICES', href: '#services' },
+  ]
+
+  const serviceItems = [
+    { name: 'WEB DESIGN', href: '/web-design-services/' },
+    { name: 'SEO', href: '/seo-services/' },
   ]
 
   return (
@@ -36,6 +42,36 @@ export default function Navbar() {
                 {item.name}
               </Link>
             ))}
+            
+            {/* Services Dropdown */}
+            <div 
+              className={styles.servicesDropdown}
+              onMouseEnter={() => setIsServicesOpen(true)}
+              onMouseLeave={() => setIsServicesOpen(false)}
+            >
+              <button className={styles.servicesButton}>
+                SERVICES
+                <svg 
+                  className={`${styles.dropdownIcon} ${isServicesOpen ? styles.dropdownIconOpen : ''}`}
+                  viewBox="0 0 20 20" 
+                  fill="currentColor"
+                >
+                  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                </svg>
+              </button>
+              
+              <div className={`${styles.dropdownMenu} ${isServicesOpen ? styles.dropdownMenuOpen : ''}`}>
+                {serviceItems.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className={styles.dropdownLink}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
           </div>
 
           {/* Contact Button */}
@@ -91,6 +127,40 @@ export default function Navbar() {
                   {item.name}
                 </Link>
               ))}
+              
+              {/* Mobile Services Dropdown */}
+              <div className={styles.mobileServicesContainer}>
+                <button 
+                  className={styles.mobileServicesButton}
+                  onClick={() => setIsMobileServicesOpen(!isMobileServicesOpen)}
+                >
+                  SERVICES
+                  <svg 
+                    className={`${styles.mobileDropdownIcon} ${isMobileServicesOpen ? styles.mobileDropdownIconOpen : ''}`}
+                    viewBox="0 0 20 20" 
+                    fill="currentColor"
+                  >
+                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                  </svg>
+                </button>
+                
+                <div className={`${styles.mobileDropdownMenu} ${isMobileServicesOpen ? styles.mobileDropdownMenuOpen : ''}`}>
+                  {serviceItems.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={styles.mobileDropdownLink}
+                      onClick={() => {
+                        setIsOpen(false)
+                        setIsMobileServicesOpen(false)
+                      }}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+              
               <Link
                 href="/contact"
                 className={styles.mobileContactButton}
